5e9494f221d42fb2c60671df921644e39bc282c9
Madalina Istrate <38633767+Madaist@users.noreply.github.com>
2019-11-12 13:56:43 +0200

    Added k-fold cross validation for knn and naive bayes

--- proiect.py
+++ proiect.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from sklearn import svm
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 
 
 PRIMELE_N_CUVINTE = 10000
@@ -41,30 +41,23 @@ def citeste_texte_din_director(cale):
         with open(fis, 'r', encoding='utf-8') as fin:
             text = fin.read()
         # text_fara_punct = re.sub("[-.,;:!?\"\'\/()_*=`]", "", text)
-        # re - regular expresion; sub - substitute # inlocuieste caracterele din [] cu ""
-        cuvinte_text = text.split()  # split - imparte dupa whitespace si da o lista de cuvinte
-        date_text.append(cuvinte_text)  # date_text contine o lista de liste de cuvinte
+        cuvinte_text = text.split()
+        date_text.append(cuvinte_text)
     return iduri_text, date_text
 
 
-### citim datele ###
 dir_path = './date_proiect/'
 labels = np.loadtxt(os.path.join(dir_path, 'labels_train.txt'))
 
 train_data_path = os.path.join(dir_path, 'train')
 iduri_train, train_data = citeste_texte_din_director(train_data_path)
-# train_data este o lista de liste de cuvinte din fiecare fisier de train
 
 # print(train_data[0][:10])  # primele 10 cuvinte din primul text
 
 test_data_path = os.path.join(dir_path, "test")
 iduri_test, test_data = citeste_texte_din_director(test_data_path)
 
-
-### numaram cuvintele din toate documentele ###
-# am numarat doar din train; daca numar si din test, nu se schimba nimic
 contor_cuvinte = defaultdict(int)
-
 for doc in train_data:
     for word in doc:
         contor_cuvinte[word] += 1
@@ -84,10 +77,9 @@ perechi_cuvinte_frecventa = perechi_cuvinte_frecventa[0:PRIMELE_N_CUVINTE]
 list_of_selected_words = []
 for cuvant, frecventa in perechi_cuvinte_frecventa:
     list_of_selected_words.append(cuvant)   # luam doar cuvintele, nu si frecventele
-### numaram cuvintele din toate documentele ###
 
 
-def get_bow(text, lista_de_cuvinte):  # face bag of words teste un singur text
+def get_bow(text, lista_de_cuvinte):  # face bag of words peste un singur text
     '''
     returneaza BoW corespunzator unui text impartit in cuvinte
     in functie de lista de cuvinte selectate
@@ -118,9 +110,6 @@ def get_bow_pe_corpus(corpus, lista):  # face bag of words pentru tot setul de d
             trebuie convertit in lista apoi in numpy.array
         '''
         v = np.array(list(bow_dict.values()))  # in v avem frecventele
-        # incercati si alte tipuri de normalizari
-        # v = v / np.sqrt(np.sum(v ** 2))
-        # v = v / np.sum(v)
         bow[idx] = v
     return bow  # va returna o matrice cu 4480 de linii si 10.000 de coloane.
     # pe fiecare linie avem frecventele corespunzatoare celor 1000 de cuvinte, din cele 4480 de texte
@@ -133,7 +122,7 @@ def confusion_matrix(predictii, labels):
     return M
 
 
-def k_fold_cross_validation(data, k = 10, C = 0.1):
+def k_fold_cross_validation_svm(data, k=10, C=0.1):
     # prepare cross validation
     kfold = KFold(k, True, 1)
     accuracies = []
@@ -155,17 +144,16 @@ def k_fold_cross_validation(data, k = 10, C = 0.1):
     return np.average(accuracies)
 
 
-print('Acuratetea medie in urma k-fold cross validation este ', k_fold_cross_validation(train_data))
+print('Acuratetea medie in urma k-fold cross validation pe SVM este ', k_fold_cross_validation_svm(train_data))
 
 
-
-#data = train_data + test_data
 data_bow_train = get_bow_pe_corpus(train_data, list_of_selected_words)
 data_bow_test = get_bow_pe_corpus(test_data, list_of_selected_words)
-# print("Data bow are shape: ", data_bow_train.shape)
+
 '''
-nr_exemple_train = 2600
-nr_exemple_valid = 200
+# Varianta cu train + validare + test
+nr_exemple_train = 2500
+nr_exemple_valid = 250
 nr_exemple_test = len(train_data) - (nr_exemple_train + nr_exemple_valid)
 nr_exemple_test_final = 1497
 
@@ -196,8 +184,8 @@ print()
 # incercati diferite valori pentru C si testati pe datele de test
 indici_train_valid = np.concatenate([indici_train, indici_valid])
 for C in [0.01, 0.1, 1, 10]:
-    clasificator = svm.SVC(C = 1, kernel = 'linear')
-    # clasificator = svm.LinearSVC(C = C, dual = False, max_iter=2983)
+    # clasificator = svm.SVC(C = 1, kernel = 'linear')
+    clasificator = svm.LinearSVC(C = C, dual = False, max_iter=2983)
     clasificator.fit(data_bow_train[indici_train_valid, :], labels[indici_train_valid])
     predictii = clasificator.predict(data_bow_train[indici_test])
     print("Acuratete pe test cu C = ", C, ": ", accuracy(predictii, labels[indici_test]))
@@ -205,11 +193,116 @@ for C in [0.01, 0.1, 1, 10]:
 
 
 ### test final ###
-#clasificator = svm.SVC(C = 1, kernel = 'linear')
+# clasificator = svm.SVC(C = 1, kernel = 'linear')
 clasificator = svm.LinearSVC(C = 1,  dual = False, max_iter=2983)  # 1 - 88%, 0.1 - 87%
 clasificator.fit(data_bow_train, labels)
 predictii_finale = clasificator.predict(data_bow_test)
 
+
+'''
+##################### knn ######################
+def k_fold_cross_validation_knn(data, k = 10, vecini = 7):
+    # prepare cross validation
+    kfold = KFold(k, True, 1)
+    accuracies = []
+    # enumerate splits
+    for train, test in kfold.split(data):
+        train_fold = np.array(data)[train]
+        test_fold = np.array(data)[test]
+        data_bow_train_fold = get_bow_pe_corpus(train_fold, list_of_selected_words)
+        data_bow_test_fold = get_bow_pe_corpus(test_fold, list_of_selected_words)
+        clasificator_knn = KNeighborsClassifier(n_neighbors= vecini, p=1)
+        clasificator_knn.fit(data_bow_train_fold, labels[train])  # antrenarea modelului
+        predictions = clasificator_knn.predict(data_bow_test_fold)  # predictiile modelului
+        acc = accuracy(predictions, labels[test])
+        accuracies.append(acc)
+        print("Acuratete pe test fold cu vecini =", vecini, ": ", acc)
+        print("Matricea de confuzie: ")
+        M = confusion_matrix(predictions, labels[test])
+        print(M)
+    return np.average(accuracies)
+
+
+print('Acuratetea medie in urma k-fold cross validation pe kNN este ', k_fold_cross_validation_knn(train_data))
+
+
+# test final:
+clasificator_knn = KNeighborsClassifier(n_neighbors= 7, p = 1)
+clasificator_knn.fit(data_bow_train, labels)
+predictii_finale_knn = clasificator_knn.predict(data_bow_test)
+'''
+
+
+'''
+# VARIANTA CU TRAIN, VALIDARE SI TEST PENTRU kNN
+neigh = KNeighborsClassifier(n_neighbors=7, p = 1)
+neigh.fit(data_bow_train[indici_train, :], labels[indici_train])
+predictii = neigh.predict(data_bow_train[indici_valid, :])
+print("Acuratete pe validare cu knn, k = 7: ", accuracy(predictii, labels[indici_valid]))
+
+# concatenam indicii de train si validare
+indici_train_valid = np.concatenate([indici_train, indici_valid])
+neigh.fit(data_bow_train[indici_train_valid, :], labels[indici_train_valid])
+predictii = neigh.predict(data_bow_train[indici_test])
+print("Acuratete pe test cu knn, k = 7 : ", accuracy(predictii, labels[indici_test]))
+
+# test final:
+neigh.fit(data_bow_train, labels)
+predictii_finale = neigh.predict(data_bow_test)
+'''
+
+'''
+################### NAIVE BAYES ###################
+def k_fold_cross_validation_bayes(data, k = 10):
+    # prepare cross validation
+    kfold = KFold(k, True, 1)
+    accuracies = []
+    # enumerate splits
+    for train, test in kfold.split(data):
+        train_fold = np.array(data)[train]
+        test_fold = np.array(data)[test]
+        data_bow_train_fold = get_bow_pe_corpus(train_fold, list_of_selected_words)
+        data_bow_test_fold = get_bow_pe_corpus(test_fold, list_of_selected_words)
+        clasificator_bayes = MultinomialNB()
+        clasificator_bayes.fit(data_bow_train_fold, labels[train])  # antrenarea modelului
+        predictions = clasificator_bayes.predict(data_bow_test_fold)  # predictiile modelului
+        acc = accuracy(predictions, labels[test])
+        accuracies.append(acc)
+        print("Acuratete pe test fold: ", acc)
+        # print("Matricea de confuzie: ")
+        # M = confusion_matrix(predictions, labels[test])
+        # print(M)
+    return np.average(accuracies)
+
+
+print('Acuratetea medie in urma k-fold cross validation pe Naive Bayes este ', k_fold_cross_validation_bayes(train_data))
+
+### test final ###
+clasificator_bayes = MultinomialNB()
+clasificator_bayes.fit(data_bow_train, labels)
+predictii_finale = clasificator_bayes.predict(data_bow_test)
+'''
+
+
+
+
+'''
+# VARIANTA CU TRAIN, TEST SI VALIDARE PENTRU NAIVE BAYES
+mnb = MultinomialNB()
+mnb.fit(data_bow_train[indici_train, :], labels[indici_train])
+predictii = mnb.predict(data_bow_train[indici_valid, :])
+print("Acuratete pe validare cu mnb: ", accuracy(predictii, labels[indici_valid]))
+
+# concatenam indicii de train si validare
+indici_train_valid = np.concatenate([indici_train, indici_valid])
+mnb.fit(data_bow_train[indici_train_valid, :], labels[indici_train_valid])
+predictii = mnb.predict(data_bow_train[indici_test])
+print("Acuratete pe test cu mnb: ", accuracy(predictii, labels[indici_test]))
+
+### test final ###
+mnb.fit(data_bow_train, labels)
+predictii_finale = mnb.predict(data_bow_test)
+'''
 
 def scrie_fisier_submission(nume_fisier, predictii, iduri):
     with open(nume_fisier, 'w') as fout:
@@ -221,7 +314,6 @@ def scrie_fisier_submission(nume_fisier, predictii, iduri):
 indici_test_final = np.arange(2984, 4481)
 scrie_fisier_submission("submisie_C10.csv", predictii_finale, indici_test_final)
 
-
 '''
 TODO pentru a face un submission pe kaggle:
     1) citesc datele de test si creez data_test_bow dupa modelul de mai sus
@@ -232,39 +324,3 @@ TODO pentru a face un submission pe kaggle:
     6) fac cross-validare: https://www.youtube.com/watch?v=sFO2ff-gTh0
     7) citesc cu atentie cerinta proiectului (!!! trebuie si un pdf cu detalii)
 '''
-
-'''
-neigh = KNeighborsClassifier(n_neighbors=7)
-neigh.fit(data_bow_train[indici_train, :], labels[indici_train])
-predictii = neigh.predict(data_bow_train[indici_valid, :])
-print("Acuratete pe validare cu knn, k = 7: ", accuracy(predictii, labels[indici_valid]))
-
-# concatenam indicii de train si validare
-indici_train_valid = np.concatenate([indici_train, indici_valid])
-neigh.fit(data_bow_train[indici_train_valid, :], labels[indici_train_valid])
-predictii = neigh.predict(data_bow_train[indici_test])
-print("Acuratete pe test cu knn, k = 7 : ", accuracy(predictii, labels[indici_test]))
-
-### test final ###
-neigh.fit(data_bow_train, labels)
-predictii_finale = neigh.predict(data_bow_test)
-'''
-'''
-gnb = GaussianNB()
-gnb.fit(data_bow_train[indici_train, :], labels[indici_train])
-predictii = gnb.predict(data_bow_train[indici_valid, :])
-print("Acuratete pe validare cu gnb: ", accuracy(predictii, labels[indici_valid]))
-
-# concatenam indicii de train si validare
-indici_train_valid = np.concatenate([indici_train, indici_valid])
-gnb.fit(data_bow_train[indici_train_valid, :], labels[indici_train_valid])
-predictii = gnb.predict(data_bow_train[indici_test])
-print("Acuratete pe test cu gnb: ", accuracy(predictii, labels[indici_test]))
-
-### test final ###
-gnb.fit(data_bow_train, labels)
-predictii_finale = gnb.predict(data_bow_test)
-'''
-
-
-
